a57f81b0fa9a0209489b18d7d8c65fec3003c543
李新阳 <lixinyangemil@outlook.com>
2020-04-18 03:46:04 +0800

    网页更改为渐进式

---
+++ js/home.js
@@ -0,0 +1,13 @@
+function createVideo(){
+    var video = document.createElement("iframe");
+    video.classList.add("modal-content");
+    video.setAttribute("width","800");
+    video.setAttribute("height","600");
+    video.setAttribute("allowfullscreen","true");
+    video.setAttribute("src","https://jx.618g.com/m3u8-dp.php?url=https://ifeng.com-v-ifeng.com/20180712/19376_e9b30d57/index.m3u8");
+    document.querySelector(".modal-dialog").appendChild(video);
+}
+
+function removeVideo(){
+    document.querySelector("iframe").remove();
+}
--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
-function createVideo(){
-    var video = document.createElement("iframe");
-    video.classList.add("modal-content");
-    video.setAttribute("width","800");
-    video.setAttribute("height","600");
-    video.setAttribute("allowfullscreen","true");
-    video.setAttribute("src","https://jx.618g.com/m3u8-dp.php?url=https://ifeng.com-v-ifeng.com/20180712/19376_e9b30d57/index.m3u8");
-    document.querySelector(".modal-dialog").appendChild(video);
+function navigation(name){
+    document.getElementById("main").innerHTML = "";
+    var url = "./view/" + name +".html";
+    $.ajax({
+        type: 'GET',
+        url: url,
+        success: function(data) {
+            document.getElementById("main").innerHTML = data;
+        },
+        error: function() {
+            alert("error");
+        }
+    });
 }
 
-function removeVideo(){
-    document.querySelector("iframe").remove();
+function init(){
+    navigation("home");
 }
