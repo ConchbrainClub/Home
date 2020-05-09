8d8d90e5f01f7c587f0f1c0e89382a58587cebae
李新阳 <lixinyangemil@outlook.com>
2020-05-10 01:58:35 +0800

    完善海螺界面

--- js/conch.js
+++ js/conch.js
@@ -1,4 +1,12 @@
 request("https://conchbrain.club/articles/page1.json",(result)=>{
     var str = JSON.stringify(result);
     console.log(str);
+
+    getNum(result);
 });
+
+function getNum(articles){
+    articles.forEach(element => {
+        console.warn(element);
+    });
+}
