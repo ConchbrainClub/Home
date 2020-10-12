2db7c0d75bd3a2cfe5bc9773ec7a16373583d2a8
lixinyang123 <lixinyangemil@outlook.com>
2020-10-12 13:11:30 +0800

    完善代码

--- js/conch.js
+++ js/conch.js
@@ -15,14 +15,17 @@ function showTimeline(){
     let endIndex = config.length-10 > 0 ? 10 : config.length;
     
     for(let i=0; i<endIndex; i++){
-        let li = document.createElement("li");
-        li.className = "list-group-item";
-        li.innerText = config[i].date;
-        li.setAttribute("onclick","loadDatePage(" + i.toString() + ")");
-        if(i==currentPage){
-            li.style.color = "red";
+
+        let style = "";
+        if(i == currentPage){
+            style = "style='color:red;'";
         }
-        list.appendChild(li);
+
+        let html = `
+            <li class="list-group-item" ${style} onclick="loadDatePage(${i})">${config[i].date}</li>
+        `;
+
+        list.innerHTML += html;
     }
 }
 
@@ -60,10 +63,13 @@ function loadData(){
 }
 
 function showDate(date){
-    let element = document.createElement("h1");
-    element.className = "display-4 m-3";
-    element.innerText = date;
-    document.querySelector("#articles").appendChild(element);
+    let html = `
+        <div class="col-md-12">
+            <h1 class="display-4 m-3">${date}</h1>
+        </div>
+    `;
+
+    document.querySelector("#articles").innerHTML += html;
 }
 
 function getNum(articles){
@@ -71,71 +77,27 @@ function getNum(articles){
 }
 
 function showData(articles){
-    let rowNum = getNum(articles);
 
-    for(let i=0;i<rowNum;i++){
-        let row = document.createElement("div");
-        row.className = "row";
+    articles.forEach(article => {
+        let html = `
+            <div class="col-md-6">
+                <div class="card mb-3">
+                    <div class="cover">
+                        <a href="${article.link}" style="background-image:url('${article.cover}');"></a>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="${article.link}">${article.title}</a>
+                        </h5>
+                        <p class="card-text">${article.desc}</p>
+                        <p class="card-text">语言：${article.language}</p>
+                    </div>
+                </div>
+            </div>
+        `;
 
-        for(let j=0;j<2;j++){
-
-            let index = i*2+j;
-            let article = articles[index];
-
-            if(index<articles.length){
-
-                let col = document.createElement("div");
-                col.className = "col-md-6";
-
-                let card = document.createElement("div");
-                card.className = "card mb-3";
-
-                let cover = document.createElement("div");
-                cover.className = "cover";
-
-                let coverlink = document.createElement("a");
-                coverlink.href = article.link;
-                coverlink.target = "blank";
-                coverlink.style.backgroundImage = "url("+ article.cover +")";
-
-                let cardBody = document.createElement("div");
-                cardBody.className = "card-body";
-
-                let title = document.createElement("h5");
-                title.className = "card-title";
-
-                let link = document.createElement("a");
-                link.href = article.link;
-                link.target = "blank";
-                link.innerText = article.title;
-                
-                let desc = document.createElement("p");
-                desc.className = "card-text";
-                desc.innerText = article.desc;
-                
-                let lang = document.createElement("p");
-                lang.className = "card-text";
-                lang.innerText = "语言：" + article.language;
-                
-                title.appendChild(link);
-
-                cardBody.appendChild(title);
-                cardBody.appendChild(desc);
-                cardBody.appendChild(lang);
-
-                cover.appendChild(coverlink);
-
-                card.appendChild(cover);
-                card.appendChild(cardBody);
-
-                col.appendChild(card);
-
-                row.appendChild(col);
-            }
-            
-            document.querySelector("#articles").appendChild(row);
-        }
-    }
+        document.querySelector("#articles").innerHTML += html;
+    });
 }
 
 function loadNext(){
